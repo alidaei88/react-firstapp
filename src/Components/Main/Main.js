71fb7146b865011f020b61aa58d72e3c11c481f7
ali Daei <ali.daei.dbm@gmail.com>
2022-03-14 18:41:13 +0330

    add limits

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,25 +5,69 @@ import "./Main.css"
 const Main = () => {
 
     const [count, setCount] = useState(10)
-
+    const [ maxLimit, setMaxLimit] = useState(19)
+    const [minLimit, setMinLimit] = useState(0)
     const [color, setColor] = useState("#ccc")
+
+    const increment = () => {
+
+        if (count >= maxLimit) setCount(count) 
+         else setCount(count+1)
+
+    } 
+
+    const decrement = () => {
+        
+        if (count <= minLimit) setCount(count)
+        else setCount(count-1)
+    }
+
+    // const backgroundCOlor = () => {
+
+    //     if (count > 15)  setColor("rgba(0,128,0, 0.5)")
+    //     else if (count < 5)  setColor("rgba(255, 0, 0, 0.5)")
+    //     else setColor("#ccc")
+
+    // }
+    
     
     useEffect(() => {
-        if (count > 15) {
-            setColor("rgba(0,128,0, 0.5)")
-        } else if (count < 5) {
-            setColor("rgba(255, 0, 0, 0.5)")
-        } else {
-            setColor("#ccc")
-          }
+
+        if (count > 15)  setColor("rgba(0,128,0, 0.5)")
+        else if (count < 5)  setColor("rgba(255, 0, 0, 0.5)")
+        else setColor("#ccc")
     }, [count])
 
+
+    const changeMaxHandler = (value) => {
+
+        setMaxLimit(value)
+    }
+
+
+    const changeMinHandler = (value) => {
+
+        setMinLimit(value)
+    }
+
+
     return(
-        <div className="main" style={{ backgroundColor: color }}>
-            <Button style={ {backgroundColor: "green"}} onClick={ () => setCount(count+1) } > + </Button>
-            <span className="counter" > { count } </span>
-            <Button style={ {backgroundColor: "red"} } onClick={ () => setCount(count-1) }> - </Button>
+        <>
+        <div>
+            <label htmlFor="">Max Limit</label>
+            <input type="number" onChange={(e) => changeMaxHandler(e.target.value)} ></input>
+            <br></br>
+            <label htmlFor="">Min limit</label>
+            <input type="number" onChange={(e) => changeMinHandler(e.target.value) } />
         </div>
+        <div className="main" style={{ backgroundColor: color }}>
+            <Button style={ {backgroundColor: "green"}} onClick={ increment } > + </Button>
+            <span className="counter" > { count } </span>
+            <Button style={ {backgroundColor: "red"} } onClick={ decrement }> - </Button>
+        </div>
+
+        </>
+        
     );
 
 }
